a78279893cf8be180d5689ec624c12e48ea79470
prushton2 <46426283+prushton2@users.noreply.github.com>
2023-06-05 17:58:27 -0400

    addit 8 bit dlatch

---
+++ src/server/Concatenator.cs
@@ -0,0 +1,36 @@
+﻿using LogicAPI.Server.Components;
+using LogicAPI.Server;
+using LogicLog;
+using LogicWorld.Server.Circuitry;
+using System;
+
+namespace prArithmetic
+{
+    public class Concatenator : LogicComponent
+    {
+        protected override void DoLogicUpdate()
+        {
+            //  input
+            // 0-7 is a
+            // 8 is s
+
+            //  output
+            // 0-7 is outputs
+
+            if(Inputs[8].On) {
+                for(int i = 0; i<8; i++) {
+                    Outputs[i].On = Inputs[i].On;
+                }
+            }
+        }
+
+        private void Add(int index, ref bool carry)
+        {
+            bool a = Inputs[index].On;
+            bool b = Inputs[index + 8].On;
+            bool ab = a ^ b;
+            Outputs[index].On = ab ^ carry;
+            carry = (ab & carry) | (a & b);
+        }
+    }
+}
